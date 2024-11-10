994e039df73138b61bf8b6c63e1e32844dc7471d
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-10 20:03:58 -0300

    create logic to search data from API and error treatment to user

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -9,15 +13,31 @@ import { Observable } from 'rxjs';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.openError("Erro ao carregar cursos.")
+          console.log(error);
+          return of([]);
+        })
+      );
   }
 
   ngOnInit(): void {
   
+  }
+
+  openError(error: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: error
+    });
   }
 
 }
