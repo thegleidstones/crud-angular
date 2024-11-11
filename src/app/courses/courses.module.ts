532a51511830d6553e9ea037f6fe0642a46fa1ff
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-11 18:15:39 -0300

    add new modules to course module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseFormComponent
   ],
   imports: [
+    AppMaterialModule,
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule,
+    ReactiveFormsModule,
     SharedModule
   ]
 })
