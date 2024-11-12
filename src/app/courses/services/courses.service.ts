b8b6336586559041da339b13e0b95bb5b728b711
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-12 01:49:10 -0300

    create methods to save and update and logics

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -11,19 +11,39 @@ import { Observable } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = 'api/courses';  
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
   list(): Observable<Course[]> {
     return this.httpClient.get<Course[]>(this.API)
-    .pipe(      
+    .pipe(
       first(),
-      tap(courses => console.log(courses))
     ) ;
   }
 
+  listById(id: string): Observable<Course> {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
   save(record: Partial<Course>): Observable<Course> {
+    console.log(record);
+
+    if (record._id) {
+      console.log('update');
+      return this.update(record);
+    }
+
+    console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>): Observable<Course> {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
+
+  private update(record: Partial<Course>): Observable<Course> {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
 }
