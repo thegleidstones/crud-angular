7230740967fac0c5cf6d9c86e115c023c521a670
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-12 14:25:36 -0300

    add confirmation dialog component on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
-    CategoryPipe
+    CategoryPipe,
+    ConfirmationDialogComponent
   ],
   exports: [
+    CategoryPipe,
+    ConfirmationDialogComponent,
     ErrorDialogComponent,
-    CategoryPipe
   ],
   imports: [
     AppMaterialModule,
