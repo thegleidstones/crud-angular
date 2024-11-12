bff3a23a5b8c27dfcf802741919b43ffb0c0ba82
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-12 14:26:28 -0300

    refactor and create logic to delete component using confirmation dialog

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -14,29 +16,23 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses$: Observable<Course[]>;
+  courses$: Observable<Course[]> = new Observable<Course[]>();
 
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
   ) {
-    this.courses$ = this.coursesService.list()
-      .pipe(
-        catchError(error => {
-          this.openError("Erro ao carregar cursos.")
-          console.log(error);
-          return of([]);
-        })
-      );
+      this.refresh();
   }
 
   ngOnInit(): void {
 
   }
 
-  openError(error: string) {
+  onError(error: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: error
     });
@@ -48,6 +44,40 @@ export class CoursesComponent implements OnInit {
 
   onEdit(course: Course) {
     this.router.navigate(['edit', course._id], {relativeTo: this.route});
+  }
+
+  onDelete(course: Course) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Tem certeza que deseja remover esse curso?',
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.coursesService.delete(course._id).subscribe(
+          () => this.refresh(),
+          () => this.onError('Erro ao remover curso.'),
+          () => {
+            this.snackBar.open('Curso removido com sucesso', 'X', {
+              duration: 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+            });
+          },
+        );
+      }
+    });
+  }
+
+  private refresh() {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(
+        error => {
+          this.onError('Erro ao carregar cursos');
+          return of([]);
+        }
+      )
+    )
   }
 
 }
