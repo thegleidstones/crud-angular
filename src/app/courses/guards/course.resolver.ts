3baadbeffba2eb01b898cc78f0239cb946903860
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-14 00:43:19 -0300

    add lessons property on resolver

--- src/app/courses/guards/course.resolver.ts
+++ src/app/courses/guards/course.resolver.ts
@@ -19,6 +19,6 @@ export class CourseResolver implements Resolve<Course> {
     if (route.params && route.params['id']) {
       return this.service.listById(route.params['id']);
     }
-    return of({_id: '', name: '', category: ''});
+    return of({_id: '', name: '', category: '', lessons: []});
   }
 }
